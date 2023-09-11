c14f649f261b11ff766aa93f4f389ac40ab8aa9a
lukeshm <zilean94@gmail.com>
2023-09-10 21:52:19 -0300

    Cambios de CSs

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -8,12 +8,16 @@ const LandingPage = () => {
    
     
     <div className={styles.container}>
+    <div className={styles.videogamesBackground}>
+    <br></br>
+    <br/>
+    <br/>
     <h1 className={styles.title}></h1>
     <Link to={`/videogames`}>
     <button className='enter-btn'>Enter</button>
     </Link>
     </div>
-    
+    </div>
   )
 }
 
